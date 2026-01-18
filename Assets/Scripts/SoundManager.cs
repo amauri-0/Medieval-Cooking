d2bd121df9beb8aa9ea6f73a166018d38cd83710
Henrique-Segundo <henriquesegundo2000@gmail.com>
2026-01-18 18:08:02 -0300

    OptionsUI and AudioLevels

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
     public static SoundManager Instance {get; private set;}
     
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    //necessario para as opcoes
+    private float volume =1f;
+
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
     private void Start()
     {
@@ -60,13 +67,30 @@ public class SoundManager : MonoBehaviour
     }
 
     //Um som
-  private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+  private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplayer = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position,volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplayer * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    //opcoes envolvendo audio
+    public void ChangeVolume(){
+        volume += .1f;
+        if(volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
     }
 }
